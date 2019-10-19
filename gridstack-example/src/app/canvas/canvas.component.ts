7277d1c0feef8f7fba495ce5b48c8c14704db5cf
Paul Sterl <paul@sterl.org>
2019-10-19 16:13:14 +0200

    added gradient // not active

--- gridstack-example/src/app/canvas/canvas.component.ts
+++ gridstack-example/src/app/canvas/canvas.component.ts
@@ -53,16 +53,26 @@ export class CanvasComponent implements OnInit, AfterContentInit {
     context.lineWidth = Math.min(canvas.width, canvas.height) / 7;
     // https://www.w3schools.com/graphics/canvas_gradients.asp
     context.strokeStyle = '#ae2424'; //'#ad2323';
-    context.shadowColor = '#656565'; //'#f1cbdb';
+    context.shadowColor = '#343a40'; //'#656565'; //'#f1cbdb';
     context.globalAlpha = 0.7;
 
     const step = 24;
     const oneX = canvas.width / step; // one step in X
     const oneY = canvas.height / step; // one step in Y
-    const startX = oneX * 4;
+    const startX = oneX * 5;
     const startY = oneY * 8;
     const endX = oneX * 22;
     const endY = oneY * 21;
+
+    // fade to bottom
+    //const grd = context.createLinearGradient(endX, startY, endX + oneX * 3, endY + oneY * 3);
+    // fade to Top
+    // const grd = context.createLinearGradient(endX, endY, endX, startY - oneY * step / 2);
+    // https://www.w3schools.com/colors/colors_picker.asp
+    // grd.addColorStop(0, '#ae2424');
+    // grd.addColorStop(1, '#fbeaea');
+    // context.strokeStyle = grd;
+
 
     context.lineWidth = Math.min(oneX, oneY) * 3;
     context.shadowBlur = context.lineWidth / 3;
@@ -85,13 +95,13 @@ export class CanvasComponent implements OnInit, AfterContentInit {
 
       // --->
       context.lineTo(endX - oneX * 2, endY);
-      context.stroke();
+      //context.stroke();
 
       // >
-      context.beginPath();
-      context.moveTo(endX - oneX * 2, endY);
-      context.lineTo(endX - oneX * 1.9, endY);
-      context.lineTo(endX - oneX * 4, endY - oneY * 2);
+      //context.beginPath();
+      //context.moveTo(endX - oneX * 2, endY);
+      context.lineTo(endX - oneX * 1.99, endY);
+      context.lineTo(endX - oneX * 3.5, endY - oneY * 2);
       context.stroke();
       context.closePath();
     }
