bd4a3c6585ee2260095f9ed81a9289a5afbb541c
Paul Sterl <paul@sterl.org>
2020-11-22 20:36:25 +0100

    using also https://github.com/sterlp/jdbc-identity-store for spring

--- spring-jdbc-security/src/main/java/org/sterl/training/spring/jdbcsecurity/springjdbcsecurity/JdbcSecurityConfig.java
+++ spring-jdbc-security/src/main/java/org/sterl/training/spring/jdbcsecurity/springjdbcsecurity/JdbcSecurityConfig.java
@@ -1,16 +1,30 @@
 package org.sterl.training.spring.jdbcsecurity.springjdbcsecurity;
 
+import java.time.Duration;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.sterl.spring.hash.SpringBCryptPbkdf2PasswordHash;
+import org.sterl.identitystore.api.IdentityStore;
+import org.sterl.identitystore.api.VerificationResult;
+import org.sterl.identitystore.api.VerificationResult.Status;
+import org.sterl.identitystore.builder.IdentityStoreBuilder;
 
 @Configuration
 @EnableGlobalMethodSecurity(
@@ -25,14 +39,49 @@ public class JdbcSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth)
       throws Exception {
+
+        final IdentityStore identityStore = IdentityStoreBuilder.jdbcBuilder(dataSource)
+            .withPasswordQuery("select password from users where enabled = true AND username = ?")
+            .withGroupsQuery("select authority from authorities where username = ?")
+            .withGroupPrefix("ROLE_")
+            .withCache(Duration.ofMinutes(15))
+            .withCachedPassword(true)
+            .build();
+
+        auth.authenticationProvider(new AuthenticationProvider() {
+            @Override
+            public boolean supports(Class<?> authentication) {
+                return UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication);
+            }
+            
+            @Override
+            public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+                final UsernamePasswordAuthenticationToken auth = (UsernamePasswordAuthenticationToken)authentication;
+                final VerificationResult verificationResult = identityStore.verify(auth.getName(), 
+                        auth.getCredentials() == null ? null : auth.getCredentials().toString());
+                
+                if (verificationResult.getStatus() == Status.VALID) {
+                    final List<SimpleGrantedAuthority> authorities = verificationResult.getGroups().stream().map(g -> new SimpleGrantedAuthority(g)).collect(Collectors.toList());
+                    return new UsernamePasswordAuthenticationToken(
+                            authentication.getPrincipal(), 
+                            authentication.getCredentials(), 
+                            authorities);
+                } else {
+                    throw new BadCredentialsException("Wrong user name or password.");
+                }
+            }
+        });
+
+        /* spring build in JDBC identity store
         auth.jdbcAuthentication()
             .rolePrefix("ROLE_") // in spring a role needs this prefix
             .dataSource(this.dataSource);
+            */
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new SpringBCryptPbkdf2PasswordHash();
+        return new BCryptPasswordEncoder(); // new SpringBCryptPbkdf2PasswordHash();
     }
 
     
