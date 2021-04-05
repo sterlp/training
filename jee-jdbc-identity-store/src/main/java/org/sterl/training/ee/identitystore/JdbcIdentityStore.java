2a2e4d51082d37fae53b71f691dadde010ddd380
Paul Sterl <paul@sterl.org>
2021-04-05 15:46:12 +0200

    added hint for payara 4

--- jee-jdbc-identity-store/src/main/java/org/sterl/training/ee/identitystore/JdbcIdentityStore.java
+++ jee-jdbc-identity-store/src/main/java/org/sterl/training/ee/identitystore/JdbcIdentityStore.java
@@ -2,6 +2,8 @@ package org.sterl.training.ee.identitystore;
 
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.inject.Singleton;
@@ -25,12 +27,20 @@ public class JdbcIdentityStore implements IdentityStore {
     }
     
     private CredentialValidationResult validate(String user, String password) {
-        final VerificationResult vr = is.verify(user, password);
-        if (vr.getStatus() == VerificationResult.Status.VALID) {
-            return new CredentialValidationResult(user, vr.getGroups());
-        } else {
-            return CredentialValidationResult.INVALID_RESULT;
+        try {
+            // if (1 == 1) throw new RuntimeException("Not today!");
+
+            final VerificationResult vr = is.verify(user, password);
+            if (vr.getStatus() == VerificationResult.Status.VALID) {
+                return new CredentialValidationResult(user, vr.getGroups());
+            } else {
+                return CredentialValidationResult.INVALID_RESULT;
+            }
+        } catch (Exception e) {
+            // NOTE: with payara 4 manuall logging is needed
+            // this is fixed in newer payara 5 versions, otherwise this exception is lost
+            Logger.getLogger(this.getClass().getSimpleName()).log(Level.SEVERE, "Login failed", e);
+            throw e;
         }
-        
     } 
 }
