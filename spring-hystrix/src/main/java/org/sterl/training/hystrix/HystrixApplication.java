44cf37843563205139f5ebd6d31703633906c044
Paul Sterl <paul.sterl@outlook.de>
2021-05-31 22:46:04 +0200

    using spring dependencies again

--- spring-hystrix/src/main/java/org/sterl/training/hystrix/HystrixApplication.java
+++ spring-hystrix/src/main/java/org/sterl/training/hystrix/HystrixApplication.java
@@ -2,7 +2,9 @@ package org.sterl.training.hystrix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
+@EnableHystrix
 @SpringBootApplication
 public class HystrixApplication {
 
